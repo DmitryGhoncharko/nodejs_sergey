cf6cee0fbb8552cb22b6f7bee9671a5aaac79316
dangeonMaster <dafae722@gmail.com>
2023-04-15 13:45:56 +0300

    fix passport.js

--- middleware/passport.js
+++ middleware/passport.js
@@ -1,7 +1,8 @@
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
 const keys = require('../config/keys')
-const  mongoose = mongoose.model('mongoose')
+
+const  mongoose = require('mongoose')
 const  User = mongoose.model('users')
 const options = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
